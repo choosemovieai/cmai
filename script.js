6228e71016930b2648099ce5feacfb333f630378
choosemovieai <choosemovieai@gmail.com>
2025-02-02 16:22:19 +0400

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,39 @@ document.getElementById('searchButton').addEventListener('click', function() {
         return;
     }
 
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=YOUR_TMDB_API_KEY&query=${query}`)
+    fetch(`http://www.omdbapi.com/?t=Interstellar&apikey=free`)
     .then(response => response.json())
     .then(data => {
-        const movies = data.results.map(movie => ({
-            title: movie.title,
-            description: movie.overview,
-            genres: movie.genre_ids, // Нужно преобразовать ID в названия жанров
-            rating: movie.vote_average,
-            age_restriction: "N/A", // TMDb не предоставляет возрастной рейтинг напрямую
-            poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-        }));
-        displayMovies(JSON.stringify(movies));
+        if (data.Response === "True") {
+            const movie = {
+                title: data.Title,
+                description: data.Plot,
+                genres: data.Genre.split(", "), // Разделяем жанры по запятой
+                rating: data.imdbRating,
+                age_restriction: data.Rated, // Возрастной рейтинг из OMDb
+                poster_url: data.Poster
+            };
+            displayMovies(JSON.stringify([movie])); // Оборачиваем в массив, чтобы совместить с форматом
+        } else {
+            console.error('Error: Movie not found');
+        }
     })
     .catch(error => console.error('Error fetching data:', error));
+
+    // fetch(`https://api.themoviedb.org/3/search/movie?api_key=YOUR_TMDB_API_KEY&query=${query}`)
+    // .then(response => response.json())
+    // .then(data => {
+    //     const movies = data.results.map(movie => ({
+    //         title: movie.title,
+    //         description: movie.overview,
+    //         genres: movie.genre_ids, // Нужно преобразовать ID в названия жанров
+    //         rating: movie.vote_average,
+    //         age_restriction: "N/A", // TMDb не предоставляет возрастной рейтинг напрямую
+    //         poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+    //     }));
+    //     displayMovies(JSON.stringify(movies));
+    // })
+    // .catch(error => console.error('Error fetching data:', error));
 
     // Отправляем запрос на мое API
     // fetch(`https://api.deepseek.com/v1/chat/completions`, {
